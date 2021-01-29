6c4a58f22073fde3eb4d5fdc0f38659fd377c888
Joe <jparkkennaby@gmail.com>
2021-01-29 22:34:12 +0000

    10 - add DB query builder documentation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+
 class PostsController extends Controller
 {
     public function show($slug) {
 
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        /**
+         * DB: Query Builder
+         * API for constructing safe, clean, sql queries
+         */
+
+        // post = \DB::table('posts')->where('slug', $slug)->first(); \ required if not imported at the top
+        $post = DB::table('posts')->where('slug', $slug)->first();
        
         if ($post === null) {
             abort(404, 'Sorry, post not found');
