3bf425d7cb3bba5de52302457960a91fd66c4ec4
Joe <jparkkennaby@gmail.com>
2021-02-05 20:08:15 +0000

    18 - render article data on about page dynamically using queries article data

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,19 +15,14 @@
 		</div>
 		<div id="sidebar">
 			<ul class="style1">
+			
+			@foreach ($articles as $article)
 				<li class="first">
-					<h3>Amet sed volutpat mauris</h3>
-					<p><a href="#">In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</a></p>
+					<h3>{{$article->title}}</h3>
+					<p><a href="#">{{$article->excerpt}}</a></p>
 				</li>
-				<li>
-					<h3>Sagittis diam dolor sit amet</h3>
-					<p><a href="#">In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</a></p>
-				</li>
-				<li>
-					<h3>Maecenas ac quam risus</h3>
-					<p><a href="#">In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</a></p>
-				</li>
-			</ul>
+			@endforeach
+
 			<div id="stwo-col">
 				<div class="sbox1">
 					<h2>Etiam rhoncus</h2>
