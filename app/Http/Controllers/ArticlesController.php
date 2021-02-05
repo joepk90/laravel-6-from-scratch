0f394caba1eb9fc2acccf2f0066116e8fb610ba0
Joe <jparkkennaby@gmail.com>
2021-02-05 22:11:17 +0000

    22 - add controller crud notes to Articles Controller

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,6 +5,34 @@ namespace App\Http\Controllers;
 use App\Article;
 use Illuminate\Http\Request;
 
+/**
+ * 7 restful controller actions:
+ * index: return a list
+ * show: return a single resource
+ * create: shows a view to create a resource
+ * store: persist a new resource
+ * edit: show a view to edit an existing resource
+ * update: persist the edited resource
+ * destroy: delete resource
+ * 
+ * 
+ * CRUD: create, read, update, delete
+ * 
+ * CRUD relationship to HTTP methods:
+ * GET /articles            // get articles 
+ * GET /articles/create     // get view to create new article
+ * POST /articles           // create new article
+ * GET /articles/2          // get specific article
+ * GET /articles/2/edit     // get view to edit specific article
+ * PUT /articles/2/         // edit specific article
+ * DELETE /articles/2/      // delete specific article
+ * 
+ * 
+ * Custom notes - intepretation of 7 restful contoller actions in relation to CRUD
+ * CRUD: store, show, update, destroy
+ * VIEWS: index, create, edit, 
+ */
+
 class ArticlesController extends Controller
 {
 
