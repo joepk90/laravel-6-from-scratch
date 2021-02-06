e8b9055b25beeafe43a51f284c4bb93a7b887837
Joe <jparkkennaby@gmail.com>
2021-02-06 20:17:56 +0000

    26 - add example default model query key override (not in use)

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    // overrride default key laravel uses when querying a model (default: id)
+    // example use case: using the slug as the wildcard 
+    // public function getRouteKeyName()
+    // {
+    // return 'slug'; // article::where('slug', $article)->first();
+    // }
 }
