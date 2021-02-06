0611b69d3101f2370813923beb91f5665ffb5960
Joe <jparkkennaby@gmail.com>
2021-02-06 20:32:36 +0000

    27 - clean up the article controller store method. use the Model create method to create a new articles using mass assignment

--- app/Article.php
+++ app/Article.php
@@ -6,10 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    // overrride default key laravel uses when querying a model (default: id)
-    // example use case: using the slug as the wildcard 
+
+    /**
+     * Mass Assignemnt
+     * properties are by defualt protected from mass assignment
+     * mass assignment means updating several values at once
+     * 
+     * the reason this is protected is to prevent mass unprotected updates
+     * for example logic could assign any number of potentially dangerous values to a users record
+     * User::create(request()->all()) // ['name' => 'newname', 'paying_subscriber' => true, 'admin' => true]
+     */
+
+    protected $fillable = ['title', 'excerpt', 'body'];
+    // protected $gaurded = []  // alternatively remove any protection against mass assignment
+
+
+
+    /**
+     * Override Default Query Key
+     * overrride default key laravel uses when querying a model (default: id)
+     * example use case: using the slug as the wildcard 
+     */
+
     // public function getRouteKeyName()
     // {
-    // return 'slug'; // article::where('slug', $article)->first();
+    //     return 'slug'; // article::where('slug', $article)->first();
     // }
 }
--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -68,12 +68,11 @@ class ArticlesController extends Controller
             'body' => 'required',
         ]);
 
-        $article = new Article();
-        $article->title = request('title');
-        $article->excerpt = request('excerpt');
-        $article->body = request('body');
-
-        $article->save();
+        Article::create([
+            'title' => request('title'),
+            'excerpt' => request('excerpt'),
+            'body' => request('body'),
+        ]);
 
         return redirect('/articles');
     }
