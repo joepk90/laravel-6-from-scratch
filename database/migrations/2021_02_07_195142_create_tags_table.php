4cc5670f8ee2593bc0d6bf97073dc8c73cf06889
Joe <jparkkennaby@gmail.com>
2021-02-07 20:13:18 +0000

    31 - setup tag migration and model. include article_tag table to handle the articles and tags many to many relationship

---
+++ database/migrations/2021_02_07_195142_create_tags_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        /**
+         * Pivot Table (Linking Table)
+         * 
+         * Laravel Convention:
+         * - singular
+         * - seperate by underscore
+         * - aphabetical order
+         * 
+         * articles and tags = article_tag
+         */
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('article_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
+
+            // force the combination of the article and tag id to be unique (avoid duplicates)
+            $table->unique(['article_id', 'tag_id']);
+
+            // setup foriegn key (delete article_tag record if related article or tag is deleted)
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('article_tag');
+    }
+}
