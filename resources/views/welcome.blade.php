bf74da4b912c18a50984df63cef12f21ea17846a
Joe <jparkkennaby@gmail.com>
2021-02-21 15:42:21 +0000

    34 - setup conditional logic to display users name on welcome and home view templates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,13 @@
 <div id="header-featured">
 		<div id="banner-wrapper">
 			<div id="banner" class="container">
-				<h2>Maecenas luctus lectus</h2>
+
+					@if (Auth::check())
+					<h2>Hi, {{Auth::user()->name}}</h2>
+                    @else 
+						<h2>Maecenas luctus lectus</h2>
+                    @endif
+
 				<p>This is <strong>SimpleWork</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </p>
 				<a href="#" class="button">Etiam posuere</a> </div>
 			</div>
