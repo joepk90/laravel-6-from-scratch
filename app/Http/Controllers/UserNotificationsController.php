b150ec7413c3e4852e33e8d0ba74d2b9a58ec063
Joe <jparkkennaby@gmail.com>
2021-02-21 17:50:05 +0000

    47 - mark notifications as read when queried within the UserNotificationsControllers show method

--- app/Http/Controllers/UserNotificationsController.php
+++ app/Http/Controllers/UserNotificationsController.php
@@ -8,8 +8,19 @@ class UserNotificationsController extends Controller
 {
     public function show()
     {
+
+        $notifications = auth()->user()->unreadNotifications;
+
+        // slow method to update notifications - not advised.
+        // foreach ($notifications as $notification) {
+        //     $notifications->markAsRead();
+        // }
+
+        // thenotifications extends the elequint collection class but with some extra helpers (markAsRead)
+        $notifications->markAsRead();
+
         return view('notifications.show', [
-            'notifications' => auth()->user()->notifications
+            'notifications' => $notifications
         ]);
     }
 }
