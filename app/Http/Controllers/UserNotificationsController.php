c3fe2f621f66b2fb4ce3e6f6ec844387cc9741d7
Joe <jparkkennaby@gmail.com>
2021-02-21 17:56:33 +0000

    47 - simplify UserNotificationsController show logic. use tap command to apply markAsRead as \callback function

--- app/Http/Controllers/UserNotificationsController.php
+++ app/Http/Controllers/UserNotificationsController.php
@@ -8,19 +8,8 @@ class UserNotificationsController extends Controller
 {
     public function show()
     {
-
-        $notifications = auth()->user()->unreadNotifications;
-
-        // slow method to update notifications - not advised.
-        // foreach ($notifications as $notification) {
-        //     $notifications->markAsRead();
-        // }
-
-        // thenotifications extends the elequint collection class but with some extra helpers (markAsRead)
-        $notifications->markAsRead();
-
         return view('notifications.show', [
-            'notifications' => $notifications
+            'notifications' => tap(auth()->user()->unreadNotifications)->markAsRead()
         ]);
     }
 }
