88944ee569735d51fd9567ab32fec790036cf43b
Joe <joeparkkennaby@gmail.com>
2021-02-27 16:33:23 +0000

    50 setup alternative authorization setup in ConversationBestReplyController

--- app/Http/Controllers/ConversationBestReplyController.php
+++ app/Http/Controllers/ConversationBestReplyController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Reply;
 use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Gate;
 
 class ConversationBestReplyController extends Controller
 {
     public function store(Reply $reply)
     {
         // authorize that the current user has permission to set the best reply
-
         $this->authorize('update-conversation', $reply->conversation);
+
+        // alternative approach
+        // if (Gate::denies('update-conversation', $reply->conversation())) {};
+
 
         $reply->conversation->best_reply_id = $reply->id;
         $reply->conversation->save();
